15a617cf1d0b900fae85b0945c9667354cbc9dbe
vivek pathak <vivekkpaathak@gmail.com>
2025-01-31 12:24:55 +0530

    added code of two sum

---
+++ Day02/src/main/hashmap/twosum/TwoSum.java
@@ -0,0 +1,37 @@
+package main.hashmap.twosum;
+
+import java.util.HashMap;
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+            // If the complement exists in the map, return the indices
+            if (map.containsKey(complement)) {
+                return new int[] {map.get(complement), i};
+            }
+
+            // Otherwise, store the current number and its index
+            map.put(nums[i], i);
+        }
+
+        // Return an empty array if no solution is found
+        return new int[] {};
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] result = twoSum(nums, target);
+
+        if (result.length == 0) {
+            System.out.println("No solution found.");
+        } else {
+            System.out.println("Indices: [" + result[0] + ", " + result[1] + "]");
+        }
+    }
+}
